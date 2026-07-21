fea11f3cdc4e43279899e11cb0beee242d13252d
Habibulla <habibulla9177@gmail.com>
2026-07-21 17:27:33 +0600

    server created

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,7 @@
-//Habibulla is the great person
+import { app } from './src/app.js';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
